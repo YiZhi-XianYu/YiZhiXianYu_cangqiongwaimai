b44ccec682ca67cacaa1644d9585da123610d434
YiZhiXianYu <2314770549@qq.com>
2026-05-23 11:53:56 +0800

    day02开发任务完成

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -111,7 +111,32 @@ public class EmployeeController {
     public Result startOrStop(@PathVariable("status") Integer status,Long id) {
         log.info("启用、禁用员工账号：{}，{}", status, id);
         employeeService.startOrStop(status,id);
+        return Result.success();
+    }
 
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询员工信息")
+    public Result<Employee> getById(@PathVariable Long id) {
+        log.info("根据id={}查询员工信息",id);
+        Employee employee = employeeService.getById(id);
+        return Result.success(employee);
+    }
+
+    /**
+     * 修改员工
+     * @param employeeDTO
+     * @return
+     */
+    @PutMapping
+    @ApiOperation("修改员工")
+    public Result update(@RequestBody EmployeeDTO employeeDTO) {
+        log.info("编辑员工信息：{}", employeeDTO);
+        employeeService.update(employeeDTO);
         return Result.success();
     }
 }
