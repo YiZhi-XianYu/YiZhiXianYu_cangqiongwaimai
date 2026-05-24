8d52db8340c1106c9262f928d5f8440653754f63
YiZhiXianYu <2314770549@qq.com>
2026-05-24 14:14:42 +0800

    分页查询和删除代码完成

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -12,4 +12,10 @@ public interface DishFlavorMapper {
      * @param flavors
      */
     void insertBatch(List<DishFlavor> flavors);
+
+    /**
+     * 批量删除口味数据
+     * @param ids
+     */
+    void deleteByDishId(List<Long> ids);
 }
